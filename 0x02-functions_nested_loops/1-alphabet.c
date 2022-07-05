d9c07d25fdc8c6be8f3c1850e4085f78cb1e2cbc
Poh-poh <mumbimak98@gmail.com>
2022-07-05 10:53:37 +0000

    updating 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  * main -entrypoint
  *
- * Description: 'Write a function that prints the alphabet, in lowercase, followed by a new line'
+ * Description:prints the alphabet, in lowercase \n
  *
  * Return: Always 0
  *
@@ -11,7 +11,7 @@ void print_alphabet(void)
 {
 	char c = 'a';
 
-	while(c <= 'z')
+	while (c <= 'z')
 	{
 	_putchar(c);
 	c++;
