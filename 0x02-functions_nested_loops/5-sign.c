fc92cc79432f2166ac07561ec0406b4b09fdfdee
Poh-poh <mumbimak98@gmail.com>
2022-07-05 17:58:25 +0000

    updating 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,15 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		return (0);
-	}else if (n < 0){
+		return (1);
+	}
+	else if (n < 0)
+	{
 		_putchar(45);
 		return (-1);
-	}else{
+	}
+	else
+	{
 		_putchar(48);
 		return (0);
 	}
