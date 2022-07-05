28edc9ee0211622c9058521c6fd9043a63c1456a
Poh-poh <mumbimak98@gmail.com>
2022-07-05 18:34:50 +0000

    updating 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,11 @@
  *
  * Return: last_digit
  */
-int print_last_digit(int n,last_digit)
+int print_last_digit(int n)
 {
-	last_digit = n % 10
+	int last_digit;
+
+	last_digit = n % 10;
 		if (last_digit < 0)
 		{
 			last_digit = last_digit * -1);
