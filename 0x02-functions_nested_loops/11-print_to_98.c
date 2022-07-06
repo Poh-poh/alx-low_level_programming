968493f802afe798cc2d24d9e0e32fdf5f049b2e
Poh-poh <mumbimak98@gmail.com>
2022-07-06 06:07:49 +0000

    adding 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,13 +19,13 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf ("%d,",n);
+				printf ("%d, ",n);
 			}
 		}
 	}
 	else
 	{
-		for (; n >= 98; n++)
+		for (; n >= 98; n--)
 		{
 			if (n == 98)
 			{
@@ -35,7 +35,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf ("%d,",n);
+				printf ("%d, ",n);
 			}
 		}
 	}
