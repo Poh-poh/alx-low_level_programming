bd8489129efd7f9284ecfdc3034ddff7a9439416
Poh-poh <mumbimak98@gmail.com>
2022-07-06 07:53:48 +0000

    adding 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,11 +1,8 @@
-/**
- * File: 104-fibonacci.c
- * Auth: Brennan D Baraban
- */
 #include <stdio.h>
+
 /**
- * main - Prints the first 98 Fibonacci numbers, starting with
- * 1 and 2, separated by a comma followed by a space.
+ * main - Prints the first 98 Fibonacci numbers
+ *
  * Return: Always 0.
  */
 int main(void)
