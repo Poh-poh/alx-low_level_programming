c8c2556b01426088dd269a8d70a703d158d59a1e
Poh-poh <mumbimak98@gmail.com>
2022-07-07 11:09:48 +0000

    update 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * Return: 0
+ * @n - first integer
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for(x = 0; x <= n; x++)
+		{
+			_putchar(95);
+		}
+	_putchar('\n');
+	}
+}
