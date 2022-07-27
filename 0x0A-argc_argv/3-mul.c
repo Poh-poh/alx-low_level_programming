a38e41cbdc0f13c83cb3cb3c5f9d12d5bbb2d27c
Poh-poh <mumbimak98@gmail.com>
2022-07-27 09:53:01 +0000

    argc argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Write a program that multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if program doesnt receive 2 arg
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int a, b, product;
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		product = a * b;
+		printf("%d\n", product);
+	}
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+	return (0);
+}
